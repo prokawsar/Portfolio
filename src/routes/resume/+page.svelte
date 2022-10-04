33aae856d94dd759bbf18256658ef17d8bfc7e37
kahmed-at-979455188925 <prokawsar@gmail.com>
2022-10-04 19:12:33 +0600

    mobile responsive

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -8,40 +8,42 @@
 		<div class="static flex flex-col">
 			<!-- name part -->
 			<div
-				class="bg-gray-300 w-6/12 mx-auto border-blue-300 h-16 z-20 flex justify-center items-center"
+				class="bg-gray-300 w-full md:w-6/12 mx-auto border-blue-300 h-16 z-20 flex justify-center items-center"
 			>
 				<h2 class="font-semibold text-3xl uppercase">Kawsar Ahmed</h2>
 			</div>
-			<div class="h-16 bg-blue-500 -mt-3 z-10 relative">
+			<div class="h-16 bg-blue-500 -mt-1 md:-mt-3 z-10 relative">
 				<h2 class="text-2xl text-white text-center inset-x-0 bottom-0 absolute align-baseline">
 					Software Engineer | Frontend | Svelte Expert
 				</h2>
 			</div>
 		</div>
 
-		<div class="flex flex-row">
-			<div class="flex flex-col w-1/3 justify-end border-r-2 border-r-indigo-500 bg-slate-50">
+		<div class="flex flex-col-reverse md:flex-row">
+			<div
+				class="flex flex-col md:w-1/3 justify-end md:border-r-2 md:border-r-indigo-500 bg-slate-50"
+			>
 				<!-- contact -->
 				<div class="mr-3 mt-3 flex flex-col">
-					<div class="flex justify-end">
+					<div class="flex md:justify-end">
 						<h1 class="uppercase text-xl font-semibold tracking-[.25rem]">Contact</h1>
 					</div>
-					<div class="flex flex-row m-4 mr-6 justify-end">
+					<div class="flex flex-row m-4 mr-6 md:justify-end">
 						<div>+880 1915983757</div>
 						<!-- TODOD: Icon here -->
 					</div>
 
-					<div class="flex flex-row m-4 mr-6 justify-end">
+					<div class="flex flex-row m-4 mr-6 md:justify-end">
 						<div>prokawsar@gmail.com</div>
 						<!-- TODOD: Icon here -->
 					</div>
 
-					<div class="flex flex-row m-4 mr-6 justify-end">
+					<div class="flex flex-row m-4 mr-6 md:justify-end">
 						<div class="font-semibold">Rajbari, Dhaka, Bangladesh 7710</div>
 						<!-- TODOD: Icon here -->
 					</div>
 
-					<div class="flex flex-row m-4 mr-6 justify-end">
+					<div class="flex flex-row m-4 mr-6 md:justify-end">
 						<a href="https://www.linkedin.com/in/prokawsar/" target="_blank"
 							>https://www.linkedin.com/in/prokawsar/</a
 						>
@@ -51,14 +53,14 @@
 
 				<!-- education -->
 				<div class="mr-3 mt-3 flex flex-col">
-					<div class="flex justify-end">
+					<div class="flex md:justify-end">
 						<h1 class="uppercase text-xl font-semibold tracking-[.25rem]">Education</h1>
 					</div>
 					<div class="flex flex-col m-4 mr-6">
-						<p class="text-gray-400 mb-3 flex justify-end">July 2018</p>
-						<p class="flex justify-end">1st division</p>
-						<p class="flex justify-end font-bold">Bachelor in IT</p>
-						<p class="flex justify-end italic">From University of Greenwich</p>
+						<p class="text-gray-400 mb-3 flex md:justify-end">July 2018</p>
+						<p class="flex md:justify-end">1st division</p>
+						<p class="flex md:justify-end font-bold">Bachelor in IT</p>
+						<p class="flex md:justify-end italic">From University of Greenwich</p>
 					</div>
 				</div>
 				<!-- key skills -->
@@ -69,13 +71,13 @@
 				</div>
 			</div>
 
-			<div class="flex w-2/3 justify-start">
-				<div class="w-9/12">
+			<div class="flex md:w-2/3 justify-start">
+				<div class="md:w-9/12">
 					<!-- profile -->
 					<div class="ml-3 mt-3">
 						<h1 class="uppercase text-xl font-semibold tracking-[.25rem]">Profile</h1>
 
-						<p class="mt-3 ml-6 text-justify">
+						<p class="mt-3 mr-3 md:ml-6 text-justify">
 							I have 4 years working experience in Svelte from its version 2. Have experience in
 							building component in both version. I have worked on large scale project built with
 							Sveltejs, bulma, bootstrap and other related technologies. Build admin dashboard with
