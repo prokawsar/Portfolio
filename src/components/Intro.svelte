673d004fbd4abc30f82d143a86813a57aa96f207
Kawsar Ahmed <prokawsar@gmail.com>
2022-10-05 02:30:38 -0400

    landing page wip more ui fix

---
+++ src/components/Intro.svelte
@@ -0,0 +1,37 @@
+<section id="home" class="intro-section w-full md:w-11/12 mx-auto">
+	<div class="flex flex-row text-white">
+		<!-- START THE CONTENT FOR THE INTRO  -->
+		<div class="basis-1/2 ml-4">
+			<h1 class="pl-9 text-6xl">
+				<span class="font-bold text-7xl">Hey,<br /> I'm Kawsar Ahmed</span>
+				<span class="text-4xl"> Software Engineer | Frontend | Svelte Expert </span>
+				<br />
+				<span class="text-xl -mt-10"> More than 4 years of remote job experience </span>
+			</h1>
+		</div>
+		<!-- START THE CONTENT FOR THE VIDEO -->
+		<div class="basis-1/2">
+			<div class="relative">
+				<img
+					src="images/arts/intro-section-illustration.png"
+					alt="video illutration"
+					class="ml-40"
+				/>
+				<a href="#" class="glightbox absolute top-1/2 left-1/2">
+					<!-- <span>
+						<i class="fas fa-play-circle" />
+					</span> -->
+					<span class="border-animation border-animation--border-1" />
+					<span class="border-animation border-animation--border-2" />
+				</a>
+			</div>
+		</div>
+	</div>
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"
+		><path
+			fill="#ffffff"
+			fill-opacity="1"
+			d="M0,160L48,176C96,192,192,224,288,208C384,192,480,128,576,133.3C672,139,768,213,864,202.7C960,192,1056,96,1152,74.7C1248,53,1344,107,1392,133.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+		/></svg
+	>
+</section>
