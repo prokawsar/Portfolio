c93b3020898d826e95b083182d473d5abbcb46a2
Kawsar Ahmed <prokawsar@gmail.com>
2023-04-07 10:43:16 +0600

    change adapter to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +10,9 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			runtime: 'nodejs18.x'
+		})
 	}
 };
 
