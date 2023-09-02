1bfdd0ab8c2afb81a2918310710fd0512ddcb82c
prokawsar <prokawsar@gmail.com>
2023-09-02 09:06:00 -0400

    loader with gdrive

--- src/lib/infos/resumeGDrive.svelte
+++ src/lib/infos/resumeGDrive.svelte
@@ -1,6 +1,20 @@
+<script lang="ts">
+	import Loader from '$lib/components/Loader.svelte'
+
+	let hideLoader = false
+
+	const loadFinish = () => {
+		hideLoader = true
+	}
+</script>
+
+<Loader classes={hideLoader ? 'hidden' : ''} />
+
 <iframe
 	class="h-[90vh]"
 	src="https://drive.google.com/file/d/1Jv26CbslfE-Z3WA7FcbM1HiVXg_3NZnp/preview"
 	width="1140"
+	on:load={() => loadFinish()}
 	allow="autoplay"
+	title="resume"
 />
