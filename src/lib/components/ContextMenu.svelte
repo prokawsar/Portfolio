4990dd40f54af0bf27434213a1511ff6bed68ae2
prokawsar <prokawsar@gmail.com>
2023-09-02 04:11:22 -0400

    context menu

---
+++ src/lib/components/ContextMenu.svelte
@@ -0,0 +1,160 @@
+<!-- 
+Github @dukenmarga, July 2022
+Context Menu is small menu that displayed when user right-click the mouse on browser.
+Think of it as a way to show Refresh option on Microsoft Windows when right-click on desktop.
+Known bug:
+    - If the browser loads the content for the first time, showMenu set to false.
+    Hence, we cannot get menu.h and menu.y dimension, since context menu has not been available at DOM.
+    The first right click will not shown properly when right-click occurs around the edge (bottom part
+    and right part) of the browser.
+
+Inspired from: Context Menu https://svelte.dev/repl/3a33725c3adb4f57b46b597f9dade0c1?version=3.25.0
+-->
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte'
+	import { slide } from 'svelte/transition'
+
+	// pos is cursor position when right click occur
+	let pos = { x: 0, y: 0 }
+	// menu is dimension (height and width) of context menu
+	let menu = { h: 0, w: 0 }
+	// browser/window dimension (height and width)
+	let browser = { h: 0, w: 0 }
+	// showMenu is state of context-menu visibility
+	let showMenu = false
+
+	const dispatch = createEventDispatcher()
+
+	const menuItems = [
+		{
+			name: 'experience',
+			onClick: workExperience,
+			displayText: 'Work Experience',
+			icon: 'fa-solid fa-briefcase',
+			class: '',
+			disabled: true
+		},
+		{
+			name: 'skills',
+			onClick: skills,
+			displayText: 'Skills',
+			icon: 'fa-solid fa-microchip',
+			class: '',
+			disabled: true
+		},
+		{
+			name: 'education',
+			onClick: education,
+			displayText: 'Education',
+			icon: 'fa-solid fa-book',
+			class: '',
+			disabled: true
+		},
+		// {
+		// 	name: 'hr'
+		// },
+		{
+			name: 'settings',
+			onClick: toggleTheme,
+			displayText: 'Toggle Theme',
+			icon: 'fa-solid fa-gear',
+			class: '',
+			disabled: false
+		}
+	]
+
+	function rightClickContextMenu(e: MouseEvent) {
+		showMenu = true
+		browser = {
+			w: window.innerWidth,
+			h: window.innerHeight
+		}
+		pos = {
+			x: e.clientX,
+			y: e.clientY
+		}
+
+		if (browser.h - pos.y < menu.h) pos.y = pos.y - menu.h
+		if (browser.w - pos.x < menu.w) pos.x = pos.x - menu.w
+	}
+	function onPageClick(e: Event) {
+		showMenu = false
+	}
+	function getContextMenuDimension(node: HTMLElement) {
+		// This function will get context menu dimension
+		// when navigation is shown => showMenu = true
+		let height = node.offsetHeight
+		let width = node.offsetWidth
+		menu = {
+			h: height,
+			w: width
+		}
+	}
+	function workExperience() {
+		dispatch('work')
+	}
+	function skills() {
+		dispatch('skills')
+	}
+	function education() {
+		dispatch('education')
+	}
+	function toggleTheme() {
+		dispatch('theme')
+	}
+</script>
+
+<svelte:head>
+	<!-- You can change icon sets according to your taste. Change `class` value in `menuItems` above to represent your icons. -->
+	<link
+		rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+		integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+		crossorigin="anonymous"
+		referrerpolicy="no-referrer"
+	/>
+</svelte:head>
+
+{#if showMenu}
+	<nav use:getContextMenuDimension style="position: absolute; top:{pos.y}px; left:{pos.x}px">
+		<div
+			transition:slide={{ duration: 200 }}
+			class="flex flex-col gap-1 bg-white py-2 w-44 rounded-lg outline outline-2 outline-[#D0E2FF]"
+			id="navbar"
+		>
+			<p class="text-center text-xl border-b pb-1">Menu</p>
+			<ul class="m-1">
+				{#each menuItems as item}
+					{#if item.name == 'hr'}
+						<hr />
+					{:else}
+						<li class="block list-none">
+							<button
+								class="w-full text-left text-md py-1 cursor-not-allowed {item.class}"
+								on:click={item.disabled ? () => {} : item.onClick}
+								class:cursor-not-allowed={item.disabled}
+								><i class={item.icon} />{item.displayText}</button
+							>
+						</li>
+					{/if}
+				{/each}
+			</ul>
+		</div>
+	</nav>
+{/if}
+
+<svelte:window on:contextmenu|preventDefault={rightClickContextMenu} on:click={onPageClick} />
+
+<style>
+	* {
+		padding: 1;
+		margin: 0;
+	}
+	ul li button:hover {
+		border-radius: 4px;
+		background-color: #eee;
+	}
+	ul li button i {
+		padding: 0px 15px 0px 10px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,77 @@
 <script lang="ts">
-	import Capsule from '$lib/components/Capsule.svelte';
+	import Capsule from '$lib/components/Capsule.svelte'
+	import Fa from 'svelte-fa'
+	import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+	import ContextMenu from '$lib/components/ContextMenu.svelte'
+
+	const handleMenuEvent = (e: any) => {
+		console.log(e.type)
+	}
 </script>
 
 <div
-	class="absolute flex justify-center items-center bg-gradient-to-b from-slate-100 to-gray-100 inset-0 md:h-screen mx-auto "
+	class="absolute flex justify-center items-center bg-gradient-to-b from-slate-100 to-gray-100 inset-0 md:h-screen mx-auto"
 >
-	<div class="flex flex-col md:flex-row gap-2 md:gap-5 w-full p-4 md:p-10 mx-5 md:mx-20">
-		<div class="flex flex-col justify-center gap-2 w-full">
-			<h1 class="text-6xl">Hi, <br /> I am Kawsar Ahmed</h1>
-			<h3 class="text-2xl sm:text-3xl">Software Engineer | Frontend</h3>
-			<p class="text-gray-600">
-				I'm delighted to have you here! As a dedicated Software Engineer specializing in frontend
-				development, my focus revolves around crafting top-notch web applications in any tech yet
-				specially with Svelte, SvelteKit. I take pride in writing clean and optimized code, ensuring
-				that your applications perform at their best.
-			</p>
-			<div class="flex flex-row gap-2">
-				<Capsule text="JavaScript" />
-				<Capsule text="Svelte" />
-				<Capsule text="SvelteKit" />
-				<Capsule text="Tailwind" />
-				<Capsule text="PHP" />
+	<div class="flex flex-col items-center w-full p-4 md:p-10 mx-5 md:mx-20">
+		<p class="hidden sm:flex font-roboto font-light">
+			To open the menu, employ a right-click with your mouse
+		</p>
+		<p class="sm:hidden">Tap anywhere to open menu</p>
+		<div class="flex flex-col md:flex-row gap-2 md:gap-5 w-full">
+			<div class="flex flex-col justify-center gap-2 w-full">
+				<h1 class="text-6xl">Hi, <br /> I am Kawsar Ahmed</h1>
+				<h3 class="text-2xl sm:text-3xl">Software Engineer | Frontend</h3>
+				<p class="text-gray-600">
+					I'm delighted to have you here! As a dedicated Software Engineer specializing in frontend
+					development, my focus revolves around crafting top-notch web applications in any tech yet
+					specially with Svelte, SvelteKit. I take pride in writing clean and optimized code,
+					ensuring that your applications perform at their best.
+				</p>
+				<div class="flex flex-row gap-2">
+					<Capsule text="JavaScript" />
+					<Capsule text="Svelte" />
+					<Capsule text="SvelteKit" />
+					<Capsule text="Tailwind" />
+					<Capsule text="PHP" />
+				</div>
+			</div>
+			<div
+				class="md:flex md:justify-center w-full hidden relative bg-hero-bg bg-no-repeat bg-cover"
+			>
+				<img
+					class="left-2 md:left-0 scale-75 sm:scale-75 lg:scale-100"
+					src="/images/avatar.png"
+					alt="avatar"
+				/>
+				<!-- <div class="absolute flex flex-row gap-2 left-1/4">
+					<Fa icon={faGithub} />
+					<Fa icon={faLinkedin} />
+				</div> -->
+			</div>
+
+			<div class="flex w-full relative md:hidden">
+				<svg class="" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+					<path
+						fill="#D0E2FF"
+						d="M47,-71.7C61.7,-63.6,75.2,-52,81.2,-37.5C87.2,-23,85.7,-5.4,82,10.9C78.2,27.1,72.1,42.2,62.5,55.4C53,68.6,39.9,79.9,24.8,84.2C9.8,88.4,-7.3,85.6,-23.9,80.9C-40.5,76.2,-56.7,69.7,-68.7,58.2C-80.7,46.7,-88.5,30.3,-91.4,12.9C-94.3,-4.5,-92.3,-22.9,-84,-37.2C-75.7,-51.5,-61,-61.8,-46,-69.9C-30.9,-77.9,-15.5,-83.7,0.3,-84.2C16.1,-84.6,32.2,-79.9,47,-71.7Z"
+						transform="translate(100 100)"
+					/>
+				</svg>
+
+				<img
+					class="absolute left-2 md:left-32 md:top-24 scale-75 md:scale-100"
+					src="/images/avatar.png"
+					alt="avatar"
+				/>
 			</div>
 		</div>
-		<div class="md:flex md:justify-center w-full hidden relative bg-hero-bg bg-no-repeat bg-cover">
-			<img
-				class="left-2 md:left-0 scale-75 sm:scale-75 lg:scale-100"
-				src="/images/avatar.png"
-				alt="avatar"
-			/>
-		</div>
-
-		<div class="flex w-full relative md:hidden">
-			<svg class="" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-				<path
-					fill="#D0E2FF"
-					d="M47,-71.7C61.7,-63.6,75.2,-52,81.2,-37.5C87.2,-23,85.7,-5.4,82,10.9C78.2,27.1,72.1,42.2,62.5,55.4C53,68.6,39.9,79.9,24.8,84.2C9.8,88.4,-7.3,85.6,-23.9,80.9C-40.5,76.2,-56.7,69.7,-68.7,58.2C-80.7,46.7,-88.5,30.3,-91.4,12.9C-94.3,-4.5,-92.3,-22.9,-84,-37.2C-75.7,-51.5,-61,-61.8,-46,-69.9C-30.9,-77.9,-15.5,-83.7,0.3,-84.2C16.1,-84.6,32.2,-79.9,47,-71.7Z"
-					transform="translate(100 100)"
-				/>
-			</svg>
-
-			<img
-				class="absolute left-2 md:left-32 md:top-24 scale-75 md:scale-100"
-				src="/images/avatar.png"
-				alt="avatar"
-			/>
-		</div>
+	</div>
+	<div class="hidden md:flex">
+		<ContextMenu
+			on:education={(e) => handleMenuEvent(e)}
+			on:skills={(e) => handleMenuEvent(e)}
+			on:work={(e) => handleMenuEvent(e)}
+			on:theme={(e) => handleMenuEvent(e)}
+		/>
 	</div>
 </div>
