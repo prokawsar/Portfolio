88591cce8f7314b8b9cc79a8fbab20bcf194050f
prokawsar <prokawsar@gmail.com>
2023-09-02 23:43:18 -0400

    fix colors

--- src/lib/components/WorkCard.svelte
+++ src/lib/components/WorkCard.svelte
@@ -8,11 +8,11 @@
 		responsibility = ''
 </script>
 
-<div class="text-blue-500 font-semibold mt-5 mb-3 ">{timeDuration}</div>
-<div class="font-bold dark:text-gray-500">{role} - {level}</div>
-<div class="italic dark:text-gray-500">{jobType} - {company}</div>
-<div class="italic dark:text-gray-500">Tech: {tech}</div>
-<div class="font-medium text-justify dark:text-gray-500">
+<div class="text-blue-500 font-semibold mt-5 text-lg">{timeDuration}</div>
+<div class="font-bold dark:text-slate-400 text-lg">{role} - {level}</div>
+<div class="italic dark:text-gray-400">{jobType} - {company}</div>
+<div class="italic dark:text-gray-400">Tech: {tech}</div>
+<div class="font-medium text-justify dark:text-gray-400/70">
 	<!-- list of resp -->
 	{responsibility}
 </div>
