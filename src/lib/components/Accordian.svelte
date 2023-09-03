8237929535cf34649fa4a1a9d2d7d42271fe36c0
prokawsar <prokawsar@gmail.com>
2023-09-03 02:56:01 -0400

    fix hover bg

--- src/lib/components/Accordian.svelte
+++ src/lib/components/Accordian.svelte
@@ -15,7 +15,7 @@
 >
 	<div class="flex flex-col relative">
 		<button
-			class="absolute right-1 hover:bg-gray-200 dark:hover:bg-slate-700 rounded-full p-1 px-[6px] flex items-center justify-center"
+			class="absolute right-1 hover:bg-slate-50 dark:hover:bg-slate-700 rounded-full p-1 px-[6px] flex items-center justify-center"
 			on:click={() => dispatch('close')}
 		>
 			<Fa icon={faMultiply} class="dark:text-white" />
