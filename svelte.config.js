2bfafe81a683e960432e0b4e7d1bf8d9941aac62
prokawsar <prokawsar@gmail.com>
2024-11-06 17:03:41 +0600

    Mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
-import adapter from '@sveltejs/adapter-vercel'
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess(), mdsvex()],
 
 	kit: {
 		adapter: adapter({
 			runtime: 'nodejs18.x'
 		})
-	}
-}
+	},
 
-export default config
+	extensions: ['.svelte', '.svx']
+};
+
+export default config;
